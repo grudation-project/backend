57673b76f3e1ebc2702edd1384ec65394d0a5249
Mohamed Attar <mohammedattar0100020@gmail.com>
2025-06-17 14:02:27 +0300

    fix: avatar

--- app/Helpers/ResourceHelper.php
+++ app/Helpers/ResourceHelper.php
@@ -7,7 +7,7 @@ class ResourceHelper
     public static function getFirstMediaOriginalUrl($object, string $relationName = 'avatar', string $defaultImageName = 'store.png', bool $shouldReturnDefault = true)
     {
         if ($object->relationLoaded($relationName)) {
-            return collect($object->getRelation($relationName)->first())->get('original_url') ?: ($shouldReturnDefault ? asset('/storage/default/' + $defaultImageName) : null);
+            return collect($object->getRelation($relationName)->first())->get('original_url') ?: ($shouldReturnDefault ? asset('/storage/default/' . $defaultImageName) : null);
         }
 
         return null;
