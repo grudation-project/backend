627dce62a355b6d00068fc18351ec65b87fba7f9
Mohamed Attar <mohammedattar0100020@gmail.com>
2025-06-18 15:39:08 +0300

    fix: ticket service filter

--- Modules/Ticket/Models/Filters/ServiceFilter.php
+++ Modules/Ticket/Models/Filters/ServiceFilter.php
@@ -7,7 +7,7 @@ class ServiceFilter
     public static function handle($query, $next, array $filters)
     {
         if(isset($filters['service_id'])) {
-            $query->where('service_id', $filters['service_id']);
+            $query->whereHas('section', fn($q) => $q->where('service_id', $filters['service_id']));
         }
 
         return $next($query);
